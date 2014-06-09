b2e66b5742f26b48c46f469e17fd1477e6084386
Dmitriy Zabotlin <arcolight@gmail.com>
2014-06-10 00:00:11 +0400

    command processor update

--- include/command_processor.hpp
+++ include/command_processor.hpp
@@ -8,86 +8,78 @@
 namespace ecl
 {
 
-template<typename... commands>
 class command_processor : public i_command
 {
 public:
-    command_processor(const uint8_t* const nm,
-                      const data_t*  const buf) :
-        name(nm),
+    command_processor(const data_t* const buf) :
         m_buf(buf)
     {}
 
-    bool dispatch(const uint8_t argc, const uint8_t** const argv)          const
+    virtual bool init(const uint8_t argc,
+                      const uint8_t* const* const argv)                 override
     {
         if(0 == argc) {
             return false;
         }
 
-        return call<0, commands...>(argv[0], argc - 1, argv + 1);
-    }
-
-    virtual size_t run()                                                override
-    {
-        dispatch(m_argc, m_argv);
-
-        return m_buf->size; // ?
-    }
-
-    virtual bool init(const uint8_t argc, const uint8_t** const argv)   override
-    {
         m_argc = argc;
         m_argv = argv;
 
         return true;
     }
 
-    const uint8_t* const name;
+    virtual bool dispatch()                                             override
+    {
+        return call<0, commands...>((const char* const)m_argv[0],
+                                    m_argc - 1,
+                                    m_argv + 1);
+    }
+
+    constexpr static const char* const name()
+    {
+        return NAME::name();
+    }
 
 private:
-    template<size_t COUNT>
-    void check()                                                           const
-    {}
-
     template<size_t COUNT, typename cmd, typename... tail>
-    bool call(const uint8_t* const  nm,
-              const uint8_t         argc,
-              const uint8_t** const argv)                                  const
+    bool call(const char* const           nm,
+              const uint8_t               argc,
+              const uint8_t* const* const argv)                            const
     {
-        static_assert((cmd::name != nullptr), "cmd::name is nullptr!");
+        static_assert((cmd::name() != nullptr), "cmd::name is nullptr!");
 
-        if(0 == strncmp((const char*)(cmd::name),
-                        (const char*)(nm),
-                        strlen((const char*)cmd::name)))
+        if(0 == strncmp(cmd::name(), nm, strlen(cmd::name())))
         {
             cmd c(m_buf);
 
-            // check for 'cmd' implements 'i_command' interface.
-            // Not really necessary, we don't use polymorphism.
-            static_cast<i_command*>(&c);
+            i_command* p_cmd = static_cast<i_command*>(&c);
 
-            if( ! c.init(argc, argv) ) {
+            if( ! p_cmd->init(argc, argv) ) {
+                const char* const p = "Invalid syntax.";
+
+                for(size_t i = 0; i < strlen(p); ++i) {
+                    m_buf->ptr[i] = p[i];
+                }
+
                 return false;
             }
 
-            c.run();
-
-            return true;
+            return p_cmd->dispatch();
         }
 
         return call<COUNT + 1, tail...>(nm, argc, argv);
     }
 
     template<size_t COUNT>
-    bool call(const uint8_t* const  nm,
-              const uint8_t         argc,
-              const uint8_t** const argv)                                  const
+    bool call(const char* const           nm,
+              const uint8_t               argc,
+              const uint8_t* const* const argv)                            const
     {
         (void)(nm);
         (void)(argc);
         (void)(argv);
 
-        const char* const p = "Command not found";
+        const char* const p = "Command not found.";
 
         for(size_t i = 0; i < strlen(p); ++i) {
             m_buf->ptr[i] = p[i];
@@ -96,8 +88,8 @@ private:
         return false;
     }
 
-    uint8_t   m_argc;
-    uint8_t** m_argv;
+    uint8_t               m_argc;
+    const uint8_t* const* m_argv;
 
     const data_t* const m_buf;
 };
