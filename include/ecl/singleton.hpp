36c91086477cff6b5be145f58afadcade8f81450
Заботлин Дмитрий <d.zabotlin@ntc-epsilon.com>
2014-06-24 14:28:46 +0400

    Singleton changes.

--- include/ecl/singleton.hpp
+++ include/ecl/singleton.hpp
@@ -10,13 +10,39 @@ template<typename T>
 class singleton
 {
 public:
+    virtual ~singleton() {}
+
     static T& instance()
     {
-        static T t;
-        return t;
+        static T object;
+
+        return object;
     }
+
+    // For really lazy people :)
+    static T& inst()
+    {
+        return instance();
+    }
+
+    static T* pointer()
+    {
+        return &(instance());
+    }
+
+protected:
+    singleton() {}
+
+private:
+    singleton(const singleton<T>& other)                               = delete;
+    singleton<T>& operator= (const singleton<T>& other)                = delete;
+    singleton(const singleton<T>&& other)                              = delete;
+    singleton<T>& operator= (const singleton<T>&& other)               = delete;
 };
-    
+
+template<typename T>
+using Singleton = singleton<T>;
+
 } // namespace ecl
 
 #endif // ECL_SINGLETON_HPP
