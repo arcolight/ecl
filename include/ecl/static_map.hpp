b4b1b01378025454bd914f6b56b54349c344e61b
Dmitry Zabotlin <arcolight@gmail.com>
2016-01-28 22:56:36 +0300

    Size method fix. Ctor now not constexpr, so build_tree() method can be called in ctor.

--- include/ecl/static_map.hpp
+++ include/ecl/static_map.hpp
@@ -26,10 +26,12 @@ public:
     using const_reverse_iterator = typename tree_t::const_reverse_iterator;
 
     template<typename... Args>
-    constexpr explicit static_map (mapped_type&& def, Args&&... args) :
+    explicit static_map (mapped_type&& def, Args&&... args) :
         m_nodes     { { std::forward<Args>(args)... } },
         m_not_found { std::forward<mapped_type>(def) }
     {
+        build_tree();
+
         static_assert(std::is_nothrow_default_constructible<mapped_type>::value,
             "Value type should be nothrow default constructible.");
     }
@@ -51,8 +53,6 @@ public:
 
     const mapped_type& operator[](const key_type& k)              const noexcept
     {
-        build_tree();
-
         const_iterator i = search(k);
 
         if(i == static_cast<const tree_t*>(&m_tree)->end())
@@ -65,8 +65,6 @@ public:
 
     const mapped_type& operator[](key_type&& k)                   const noexcept
     {
-        build_tree();
-
         const_iterator i = search(k);
 
         if(i == static_cast<const tree_t*>(&m_tree)->end())
@@ -77,43 +75,33 @@ public:
         return *i;
     }
 
-    constexpr static std::size_t size()                                 noexcept
+    std::size_t size()                                            const noexcept
     {
-        return N;
+        return m_tree.count();
     }
 
     const V& not_found()                                          const noexcept
     {
-        build_tree();
-
         return m_not_found;
     }
 
     const_iterator begin()                                        const noexcept
     {
-        build_tree();
-
         return static_cast<const tree_t*>(&m_tree)->begin();
     }
 
     const_iterator end()                                          const noexcept
     {
-        build_tree();
-
         return static_cast<const tree_t*>(&m_tree)->end();
     }
 
     const_reverse_iterator rbegin()                               const noexcept
     {
-        build_tree();
-
         return static_cast<const tree_t*>(&m_tree)->rbegin();
     }
 
     const_reverse_iterator rend()                                 const noexcept
     {
-        build_tree();
-
         return static_cast<const tree_t*>(&m_tree)->rend();
     }
 
@@ -144,13 +132,13 @@ template
     , typename... Args
     , typename Compare = std::less<K>
 >
-constexpr inline static_map
-                 <
-                       K
-                     , V
-                     , sizeof...(Args)
-                     , Compare
-                 > create_map(V&& def, Args&&... args)
+constexpr inline const static_map
+                       <
+                             K
+                           , V
+                           , sizeof...(Args)
+                           , Compare
+                       > create_map(V&& def, Args&&... args)
 {
     return static_map
            <
--- tests/test_static_map.cpp
+++ tests/test_static_map.cpp
@@ -15,36 +15,28 @@ enum class E
 using key_type   = E;
 using value_type = const char*;
 
-const auto m = ecl::create_map<key_type, value_type>
+auto m = ecl::create_map<key_type, value_type>
 (
     "not found"
     , std::make_pair(E::e1, "e1")
     , std::make_pair(E::e2, "e2")
-    , std::make_pair(E::e3, "e3")
+    , std::make_pair(E::e3, "_e3")
+    , std::make_pair(E::e3, "e3") // overwrite E::e3 element
 );
 
 int main(int, char**, char**)
 {
     std::cout << "map elements count: " << m.size() << std::endl;
 
-    std::cout << "range-for interation over map [second elements in pair]: ";
+    std::cout << "range-for interation over map: ";
     for(auto& p : m)
     {
         std::cout << p << " ";
     }
     std::cout << std::endl;
 
-    value_type v = nullptr;
-
-    v = m[E::e1];
-    std::cout << "map[E::e1]: " << v << std::endl;
-
-    v = m[E::e2];
-    std::cout << "map[E::e2]: " << v << std::endl;
-
-    v = m[E::e3];
-    std::cout << "map[E::e3]: " << v << std::endl;
-
-    v = m[E::e4];
-    std::cout << "map[E::e4]: " << v << std::endl;
+    std::cout << "map[E::e1]: " << m[E::e1] << std::endl;
+    std::cout << "map[E::e2]: " << m[E::e2] << std::endl;
+    std::cout << "map[E::e3]: " << m[E::e3] << std::endl;
+    std::cout << "map[E::e4]: " << m[E::e4] << std::endl;
 }
