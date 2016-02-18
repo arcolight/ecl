b71314e439cf0962a33b3252bfdec38aaa89e257
Dmitriy Zabotlin <dzabotlin@npp-crts.ru>
2016-02-18 12:54:10 +0300

    Main include file ecl.hpp fixed.

--- include/ecl/ecl.hpp
+++ include/ecl/ecl.hpp
@@ -13,14 +13,16 @@
 #include <ecl/command.hpp>
 #include <ecl/command_processor.hpp>
 #include <ecl/fsm.hpp>
-#include <ecl/logger.hpp>
 #include <ecl/name_type.hpp>
 #include <ecl/scope_guard.hpp>
 #include <ecl/singleton.hpp>
 #include <ecl/str_const.hpp>
 #include <ecl/stream.hpp>
+#include <ecl/scope_guard.hpp>
 
 #include <ecl/json.hpp>
-#include <ecl/web.hpp>
+
+#include <ecl/tree/simple_binary_tree.hpp>
+#include <ecl/tree/red_black_tree.hpp>
 
 #endif // ECL_HPP
