b9b9f0f716ec1d5b14d3a14d0d5c0d1cc2a556c0
Dmitriy Zabotlin <dzabotlin@npp-crts.ru>
2016-03-11 10:50:14 +0300

    cbegin, cend, crbegin, crend added to circular_buffer.

--- include/ecl/circular_buffer.hpp
+++ include/ecl/circular_buffer.hpp
@@ -424,6 +424,16 @@ public:
         return const_iterator(m_size, *this);
     }
 
+    const_iterator cbegin()                                       const noexcept
+    {
+        return const_iterator(0, *this);
+    }
+
+    const_iterator cend()                                         const noexcept
+    {
+        return const_iterator(m_size, *this);
+    }
+
     reverse_iterator rbegin()                                           noexcept
     {
         return reverse_iterator(end());
@@ -440,6 +450,16 @@ public:
     }
 
     const_reverse_iterator rend()                                 const noexcept
+    {
+        return const_reverse_iterator(begin());
+    }
+
+    const_reverse_iterator crbegin()                              const noexcept
+    {
+        return const_reverse_iterator(end());
+    }
+
+    const_reverse_iterator crend()                                const noexcept
     {
         return const_reverse_iterator(begin());
     }
